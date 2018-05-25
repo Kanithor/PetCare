7c54febfe9f1103ae0e226745e0b0496e6d174a7
John Bidwell <johnbidwellb@gmail.com>
2018-05-25 17:36:24 -0400

    Configuración inicial DB

--- app/setup.py
+++ app/setup.py
@@ -1,5 +1,19 @@
-host = ""
-port = ""
-user = ""
-passwd=""
-database = ""
+from config import *
+import psycopg2
+
+conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
+cur = conn.cursor()
+
+sql="""
+    DROP SCHEMA public CASCADE;
+    CREATE SCHEMA public;
+    CREATE TABLE users
+        (id SERIAL PRIMARY KEY, name VARCHAR, mail varchar, cellphone varchar, address varchar);
+    CREATE TABLE pets
+        (id SERIAL PRIMARY KEY, raza varchar, dueno_id INTEGER);
+    """
+
+cur.execute(sql)
+conn.commit()
+cur.close()
+conn.close()
