1a9c7261fd1d634d0cf5974e32519ba93e0b5a8a
Yerson Sarria Quiroz <yerson.sarria@mail.udp.cl>
2018-05-31 16:59:55 -0400

    footer fixeado, container añadido, comienzo de manejo de header

--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from flask import Flask, render_template
 from app import app
 
 @app.route('/')
+def index():
+    return render_template('layouts/base.html')
+
 @app.route('/users/login')
 def login():
-    return render_template("users/login.html")
+    return render_template('users/login.html')
     
