f77f487e8a9edb82c9d89002873250d545a19f6c
Yerson Quiroz <yerson.sarria@mail.udp.cl>
2018-06-28 00:33:08 -0400

    Minor changes en entidades BDD, readme y fix respecto a merge.

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-# Bases_Proyecto_db
-=======
-# bootstrap-tuto
+# PetCare
+Proyectos TICs I.
 
 
 ## Configuración
 
 ### Views
-Funciones que permiten insertar código python en las vistas
+Funciones que permiten insertar código python en las vistas.
 
 ### Setup
-Configuración inicial de la base de datos
-
->>>>>>> 2a7ff4b1c703d4905ad846630aeab3621467d0fa
+Configuración inicial de la base de datos.
--- app/setup.py
+++ app/setup.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from configuraciones import *
 import psycopg2
 conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
@@ -13,15 +12,15 @@ cur.execute(sql)
 sql ="""
 CREATE TABLE usuarios 
            (id serial PRIMARY KEY,rol integer, nombre varchar(40),apellido varchar(40),
-           email varchar(100),passwd varchar(255), creado timestamp);
+           email varchar(100), passwd varchar(255), telefono integer, creado timestamp);
 """
 
 cur.execute(sql)
 
 
 sql ="""
-CREATE TABLE mascota 
-           (id serial PRIMARY KEY, nombre varchar, raza varchar, creado timestamp);
+CREATE TABLE mascotas
+           (id SERIAL PRIMARY KEY, raza varchar, dueno_id INTEGER creado timestamp);
 """
 
 cur.execute(sql)
@@ -34,24 +33,3 @@ CREATE TABLE historial
 conn.commit()
 cur.close()
 conn.close()
-=======
-from config import *
-import psycopg2
-
-conn = psycopg2.connect("dbname=%s user=%s password=%s"%(database,user,passwd))
-cur = conn.cursor()
-
-sql="""
-    DROP SCHEMA public CASCADE;
-    CREATE SCHEMA public;
-    CREATE TABLE users
-        (id SERIAL PRIMARY KEY, name VARCHAR, mail varchar, cellphone varchar, address varchar);
-    CREATE TABLE pets
-        (id SERIAL PRIMARY KEY, raza varchar, dueno_id INTEGER);
-    """
-
-cur.execute(sql)
-conn.commit()
-cur.close()
-conn.close()
->>>>>>> 2a7ff4b1c703d4905ad846630aeab3621467d0fa
